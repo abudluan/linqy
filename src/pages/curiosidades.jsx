232397e6ab618d8f809cd1505e9236095bd27a20
abudluan <abudluan@gmail.com>
2025-12-11 16:10:01 -0300

    test: categoria curiosidades com imagens na pasta puiblic

--- src/pages/curiosidades.jsx
+++ src/pages/curiosidades.jsx
@@ -18,42 +18,42 @@ const Curiosidades = () => {
         {
             titulo: "The Death Clock",
             url: "https://www.death-clock.org/",
-            img: "public/curiosidades/deathClock.png",
+            img: "/curiosidades/deathClock.png",
             descricao: "Nossa calculadora avançada de expectativa de vida irá prever sua data de morte com base em estilo de vida e localização.",
             tags: ["testes"]
         },
         {
             titulo: "Floor796",
             url: "https://floor796.com/",
-            img: "public/curiosidades/floor796.png",
+            img: "/curiosidades/floor796.png",
             descricao: "Uma enorme cena animada cheia de referências a memes, jogos, filmes e cultura pop.",
             tags: ["cultura-pop"]
         },
         {
             titulo: "Neal.fun",
             url: "https://neal.fun/",
-            img: "public/curiosidades/nealFun.png",
+            img: "/curiosidades/nealFun.png",
             descricao: "Jogos, visualizações e experiências estranhas criadas por Neal Agarwal.",
             tags: ["interativos"]
         },
         {
             titulo: "The Useless Web",
             url: "https://theuselessweb.com/",
-            img: "public/curiosidades/uselessWeb.png",
+            img: "/curiosidades/uselessWeb.png",
             descricao: "Um botão que te leva a sites aleatórios, estranhos e inúteis da internet.",
             tags: ["interativos"]
         },
         {
             titulo: "PointerPointer",
             url: "https://pointerpointer.com/",
-            img: "public/curiosidades/pointerPointer.jpg",
+            img: "/curiosidades/pointerPointer.jpg",
             descricao: "Sempre mostra alguém apontando exatamente para onde está o seu mouse.",
             tags: ["interativos"]
         },
         {
             titulo: "Radio Garden",
             url: "https://radio.garden",
-            img: "public/curiosidades/radioGarden.png",
+            img: "/curiosidades/radioGarden.png",
             descricao: "Explore rádios do mundo todo girando o planeta em um globo 3D.",
             tags: ["interativos"]
         },
